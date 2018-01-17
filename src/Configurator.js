cb0fcb4df065030071bae0ca8b29db2f89726e54
itrambovetskiy <igortram@gmail.com>
2018-01-17 12:39:09 +0200

    Support of deep embedded configuration objects

--- src/Configurator.js
+++ src/Configurator.js
@@ -1,3 +1,5 @@
+const Utils = require(`./Utils`);
+
 /**
  * Configurator class
  * Reads mentioned JSON file and check for corresponding environmental variables and adds
@@ -5,19 +7,44 @@
  */
 class Configurator {
 
+    /**
+     * Creates new configuration object
+     * @param pathToConfigJson
+     * @param envPrefix
+     */
     constructor(pathToConfigJson, envPrefix = ``) {
         const me = this;
-        const configJson = require(pathToConfigJson);
 
-        Object.keys(configJson).forEach(key => {
-            const envVarName = envPrefix ? `${envPrefix}.${key}` : key;
+        me.envPrefix = envPrefix;
 
-            if (process.env[envVarName]) {
-                me[key] = process.env[envVarName] === `true` ? true :
-                    process.env[envVarName] === `false` ? false :
-                        process.env[envVarName];
+        me._spreadConfigValues(require(pathToConfigJson), me);
+    }
+
+    /**
+     * Spreads configuration values over Configurator object
+     * @param configObject
+     * @param rootObject
+     * @param rootKey
+     * @private
+     */
+    _spreadConfigValues(configObject, rootObject = {}, rootKey = ``) {
+        const me = this;
+
+        Object.keys(configObject).forEach(key => {
+            if (Utils.isObject(configObject[key])) {
+                rootObject[key] = {};
+                me._spreadConfigValues(configObject[key], rootObject[key], `${rootKey}.${key}`);
             } else {
-                me[key] = configJson[key];
+                const rootString = rootKey ? `.${rootKey}` : ``;
+                const envVarName = me.envPrefix ? `${me.envPrefix}${rootKey}.${key}` : key;
+
+                if (process.env[envVarName]) {
+                    rootObject[key] = process.env[envVarName] === `true` ? true :
+                        process.env[envVarName] === `false` ? false :
+                            process.env[envVarName];
+                } else {
+                    rootObject[key] = configObject[key];
+                }
             }
         });
     }
---
+++ src/Utils.js
@@ -0,0 +1,19 @@
+
+/**
+ * Utils class
+ */
+class Utils {
+
+    /**
+     * Returns true if "variable" is object
+     * @param variable
+     * @returns {boolean}
+     */
+    static isObject (variable) {
+        return typeof variable === `object`;
+    }
+
+}
+
+
+module.exports = Utils;
